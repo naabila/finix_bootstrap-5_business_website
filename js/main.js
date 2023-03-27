3115f266e23f1dbfb12333f0aff80b27c881ab24
Nabila Islam <nabilaislamhstu53@gmail.com>
2023-03-27 18:44:08 +0600

    hero section completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 
-new WOW().init();
+jQuery(document).ready(function () {
+    //wow js
+    new WOW().init();
+
+	//hamburger
+    $('.hamburger').click(()=>
+    $('.hamburger').toggleClass('is-active') )
+
+    
+});
+//sticky nav
+window.addEventListener('scroll',function(){
+    var header=this.document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY >200);
+})
+
+
+
+
