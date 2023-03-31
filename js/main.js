de2820adace562fb7ca060ff6289ecc092b8426b
Nabila Islam <nabilaislamhstu53@gmail.com>
2023-03-31 12:59:27 +0600

    home page completed

--- js/main.js
+++ js/main.js
@@ -6,8 +6,64 @@ jQuery(document).ready(function () {
     $('.hamburger').click(()=>
     $('.hamburger').toggleClass('is-active') )
 
+    //owl carousel
+    $('.project-slider').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        
+        responsive:{
+            0: {
+				items: 1
+			},
+			576: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1024: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+        }
+    })
+
+ //brand carousel  
+ $('.partner-slider').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    smartSpeed: 2000,
+    margin:0,
+    autoplayHoverPause: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        576: {
+            items: 1
+        },
+        768: {
+            items: 3
+        },
+        1024: {
+            items: 4
+        },
+        1200: {
+            items: 5
+        }
+    }
+});
+
     
 });
+
 //sticky nav
 window.addEventListener('scroll',function(){
     var header=this.document.querySelector("header");
